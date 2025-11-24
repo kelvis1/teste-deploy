4126a6bfda8c6f9d6c9350dc88c7593877787bd5
kelvis <kelves.rod20@gmail.com>
2025-11-24 18:54:34 -0300

    init

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import serverless from "serverless-http";
+
+const app = express();
+
+// Endpoint GET /
+app.get("/", (req, res) => {
+  res.json({
+    status: "API ONLINE",
+    hora: new Date().toISOString()
+  });
+});
+
+// Exporta como função serverless (obrigatório na Vercel)
+export const handler = serverless(app);
